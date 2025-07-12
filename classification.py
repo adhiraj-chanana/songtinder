7c84944766e66ede99623c445f3c3c05b51a7ee1
chananaa <chananaa@msu.edu>
2025-07-12 17:39:26 -0400

    created FAISS index file, running on CPU

--- classification.py
+++ classification.py
@@ -1,5 +1,10 @@
 import pandas as pd
 from transformers import pipeline
+from sentence_transformers import SentenceTransformer
+import numpy as  np
+import faiss
+
+
 
 song_data=pd.read_csv('data/spotify_songs.csv')
 
@@ -14,10 +19,13 @@ mood_labels=["happy", "sad", "energetic", "calm", "romantic",
  "dark", "uplifting", "dreamy", "aggressive", "melancholic", 
  "funky", "chill", "playful", "intense", "mellow"]
 
+
+# df_shuffled = song_data.sample(frac=1, random_state=42)
+song_info=[]
+song_sentences=[]
 classifier = pipeline("zero-shot-classification",
                       model="facebook/bart-large-mnli")
-df_shuffled = song_data.sample(frac=1, random_state=42)
-for i,row in df_shuffled.iloc[:32].iterrows():
+for i,row in song_data.iterrows():
     genre=row['playlist_genre']
     sub_genre=row['playlist_subgenre']
     playlist_name=row['playlist_name']
@@ -27,7 +35,32 @@ for i,row in df_shuffled.iloc[:32].iterrows():
     result_mood=classifier(sequence_to_classify, mood_labels)
     top_genres = result_genre['labels'][:3]
     top_moods = result_mood['labels'][:3]
-    print(top_genres, top_moods)
+    tags = top_genres + top_moods
+    sentence_to_embed = ", ".join(tags)
+    song_sentences.append(sentence_to_embed)
+    song_info.append([row["track_name"], row["track_artist"]])
+
+#Embedding the senteces of the songs
+model = SentenceTransformer('sentence-transformers/paraphrase-MiniLM-L6-v2')
+embeddings = model.encode(song_sentences)
+print(embeddings.shape)
+audio_features=[]
+for i,row in song_data.iterrows():
+    audio_features.append([row['danceability'], row['energy'], row['key']/11, (row['loudness']+60)/60, row['mode'], row['speechiness'], row['acousticness'], row['instrumentalness'], row['liveness'], row['valence'], row['tempo']])
+audio_features=np.array(audio_features,  dtype=np.float32)
+combined_vector = np.hstack([embeddings, audio_features])
+print(combined_vector.shape)
+norms = np.linalg.norm(combined_vector, axis=1, keepdims=True)
+normalized_vectors = combined_vector / norms
+index = faiss.IndexFlatIP(395)
+index.add(normalized_vectors)
+print(index.ntotal)
+# print(song_sentences)
+# print(song_info)
+
+
+
+
 
 
 
